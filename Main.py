a8000c93568e97050668d52d68bc2d812cf6e78b
gunjalshubhangi36-rgb <gunjalshubhangi36@gmail.com>
2026-09-17 15:14:49 +0530

    middleware added

--- Main.py
+++ Main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from Routes.StudentRoutes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app  = FastAPI()
 
 app.include_router(router)
+
+app.add_middleware(
+CORSMiddleware,
+allow_credentials=True,
+allow_headers=["*"],
+allow_methods=["*"],
+allow_origin=["*"]
+)
 
 @app.get("/")
 def greet():
