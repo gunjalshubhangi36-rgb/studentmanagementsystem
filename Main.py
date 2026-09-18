6c5c084df9655a916ae2b9c5f006d02758097e39
gunjalshubhangi36-rgb <gunjalshubhangi36@gmail.com>
2026-09-18 12:56:29 +0530

    cors updated

--- Main.py
+++ Main.py
@@ -4,11 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app  = FastAPI()
 
+origin=[
+    "http://localhost:5173/"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_origin=["*"],
-    access_control_allow_origin=["*"],
+    allow_origins=origin,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
